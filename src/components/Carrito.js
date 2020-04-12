218fb2f872ef3cb6dabdd635e66b79f403da6876
Migue Arias <aria4016@siigo.com>
2020-04-11 19:21:47 -0500

    css  Change

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './carrito.css';
 import Producto from './Producto';
 
-
-
 const Carrito = ({carrito, agregarProducto}) => ( 
   <div className="carrito">
       <h2>Tu carrito de compras</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 function Header({titulo}){
 
-  
   return (
   <h1 className="encabezado"> {titulo}</h1>
   )
